a87f01aa15eac8bcd1dad5ded584f2b59406126d
Nicholas Petty <nick@ihackeverything.com>
2016-05-06 20:46:28 -0500

    Works!

--- brain.py
+++ brain.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import time
 
 class BrainfuckInt:
 	
@@ -19,18 +20,26 @@ class BrainfuckInt:
 				temp.append(x)
 		return temp
 
+	def debug(self):
+		print str(self.mem[0]) + '|' + str(self.mem[1]) + '|' + str(self.mem[2]) + '|' + str(self.mem[3]) + '|' + str(self.mem[4]) + '|' + str(self.mem[5]) + '|' + str(self.mem[6])
+
 	def parse(self, x):
 		if x == '>': # Move pointer forward
 			self.pntr += 1
 
 		if x == '<': # Move pointer back
+			#print 'called'
 			self.pntr -= 1
 
 		if x == '+': # Increment current byte pos by 1
+			#self.debug()
 			self.mem[self.pntr] += 1
 
 		if x == '-': # Deincrement current byte pos by 1
-			self.mem[self.pntr] -= 1
+			#self.debug()
+			#print self.mem[self.pntr]
+			self.mem[self.pntr] += -1
+			#print self.mem[self.pntr]
 
 		if x == '.': # Print hex value for byte pos
 			print chr(self.mem[self.pntr])
@@ -39,25 +48,28 @@ class BrainfuckInt:
 			i = raw_input
 			self.mem[self.pntr] = i
 
-		if x == '[': # Begin while loop
-			self.tmpPntr = self.pntr
-			self.codePntr += 1
-			eol = int
+		if x == '[':
+			m = self.pntr
 			tcp = self.codePntr
+			loopend = int
 
-			while self.mem[self.tmpPntr] != 0:
-				i = self.bfCode[self.codePntr]
-				
-				if i == ']':
-					self.codePntr = tcp
-				
-				self.parse(i)
+			while self.mem[m] != 0:
 				self.codePntr += 1
-		
+				if self.bfCode[self.codePntr] == ']':
+					loopend = self.codePntr			
+					self.codePntr = tcp
+				else:
+					self.parse(self.bfCode[self.codePntr])
+
+			if self.mem[m] == 0:
+				#print 'loopend'
+				self.codePntr = loopend
+
 	def start(self):
 		while self.codePntr < len(self.bfCode):
 			self.parse(self.bfCode[self.codePntr])
 			self.codePntr += 1
+
 
 if len(sys.argv) > 1:
 	bfFile = sys.argv[1]
